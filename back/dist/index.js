5878604a5f594d77f1aa5000476270688b6933cd
ABengelsdorff <Angelica.bengelsdorff.5@gmail.com>
2024-09-26 12:09:55 -0300

    homework L2 (TypeScript II)

---
+++ back/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function calcularResultado() {
+    const num1 = 15;
+    const num2 = 15;
+    const resultado = num1 + num2;
+    console.log(resultado);
+}
+calcularResultado();
---
+++ back/src/index.ts
@@ -0,0 +1,8 @@
+function calcularResultado() {
+    const num1: number = 15;
+    const num2: number = 15;
+    const resultado: number = num1 + num2;
+    console.log(resultado);
+}
+
+calcularResultado();
